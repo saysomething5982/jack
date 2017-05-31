a9f2a54fdb946f10636c25df025e733db1e5fa5a
jatinder <jatinderjit121@gmail.com>
2017-05-31 06:27:51 +0100

    skill page design is done need to add some content

---
+++ skill.php
@@ -0,0 +1,305 @@
+<?php
+
+require('header.php');
+?>
+
+<div class="container">
+
+
+<div class="row">
+       <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#java"><i class="fa fa-angle-down"></i><span>PHP & MySql</span></button></div>
+
+
+         	 <div id="java" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#htmlcss"><i class="fa fa-angle-down"></i><span>HTML & CSS</span></button></div>
+
+
+         	 <div id="htmlcss" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#bootstrap"><i class="fa fa-angle-down"></i><span>BootStrap</span></button></div>
+
+
+         	 <div id="bootstrap" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    </div>
+
+
+
+
+<div class="row">
+       <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#git"><i class="fa fa-angle-down"></i><span>Git & GitHub</span></button></div>
+
+
+         	 <div id="git" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#wordpress"><i class="fa fa-angle-down"></i><span>WordPress</span></button></div>
+
+
+         	 <div id="wordpress" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#javascript"><i class="fa fa-angle-down"></i><span>JavaScript</span></button></div>
+
+
+         	 <div id="javascript" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    </div>
+
+
+
+<div class="row">
+       <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#jquery"><i class="fa fa-angle-down"></i><span>jQuery</span></button></div>
+
+
+         	 <div id="jquery" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#ajax"><i class="fa fa-angle-down"></i><span>Ajax</span></button></div>
+
+
+         	 <div id="ajax" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#photoshop"><i class="fa fa-angle-down"></i><span>PhotoShop</span></button></div>
+
+
+         	 <div id="photoshop" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    </div>
+
+
+<div class="row">
+       <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#rwd"><i class="fa fa-angle-down"></i><span>Responsive Design</span></button></div>
+
+
+         	 <div id="rwd" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#sass"><i class="fa fa-angle-down"></i><span>SASS</span></button></div>
+
+
+         	 <div id="sass" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#site"><i class="fa fa-angle-down"></i><span>Sublime-Text</span></button></div>
+
+
+         	 <div id="site" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    </div>
+
+<div class="row">
+       <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#seo"><i class="fa fa-angle-down"></i><span>On-Page-SEO</span></button></div>
+
+
+         	 <div id="seo" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#smo"><i class="fa fa-angle-down"></i><span>SMO</span></button></div>
+
+
+         	 <div id="smo" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    
+
+     <div class="col-sm-4">
+            <div class="home">
+		<div class="home_address">	
+                 <div class="address_title">  <button  class="my-btn btn btn-primary"  data-toggle="collapse" data-target="#site"><i class="fa fa-angle-down"></i><span>Site Maintenance</span></button></div>
+
+
+         	 <div id="site" class="collapse">
+                         <br>  
+			<span> GitHub - </span> <i class="fa fa-github" ></i><br><br><br>
+			<span>LinkedIn - </span><i class="fa fa-linkedin-square" ></i><br><br><br>
+    		        <span>Facebook - </span><i class="fa fa-facebook-square" ></i><br><br><br>
+    		        <span>twitter - </span><i class="fa fa-twitter-square" ></i><br><br>
+		</div>
+		</div>
+	    </div>
+       </div>
+    </div>
+
+
+
+
+
+
+</div>
+
+<?php
+
+require('footer.php');
+?>
