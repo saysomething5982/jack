e8c281ad0134578974fffdeee745a8c64778d80b
jatinder <jatinderjit121@gmail.com>
2017-05-31 06:34:17 +0100

    contact page is completed and .gitignore file is edited

---
+++ contact.php
@@ -0,0 +1,54 @@
+<?php 
+require("header.php");
+?>
+
+<div class="container">
+	
+	<div class="row">
+		<div class="col-sm-4">
+		<div class="skill">
+			<div class="skill_address">
+				<div class="address_title">HOME ADDRESS</div>
+				
+				<br>
+				387, ILFORD LANE<br><br>
+				ILFORD<br><br>
+				IG1 2SL<br><br>
+				ESSEX<br><br>
+				LONDON , UK <br><br>
+				
+				
+
+			</div>
+		</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="skill">
+		<div class="skill_address">
+		<div class="address_title">MY ONLINE HOME</div>
+			<br>
+			<a href="https://github.com/saysomething5982/restaurant"><span>GitHub.com - </span>  <i class="fa fa-github"></i></a><br><br><br>
+                                           
+			<a href="https://www.linkedin.com/in/jatinderjitprofile/"><span>linkedIn.com - </span>  <i class="fa fa-linkedin-square"></i></a><br><br><br>
+    		        <a href="https://www.facebook.com/jatinderjit.singh.35"><span>facebook.com - </span>  <i class="fa fa-facebook-square"></i></a><br><br><br>
+    		        <a href="https://twitter.com/jatinderjit_121"><span>twitter.com - </span>  <i class="fa fa-twitter-square"></i></a><br>
+			
+		</div>
+		</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="skill">
+		<div class="skill_address">
+		<div class="address_title">ON GOOGLE MAP</div>
+
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2481.1120148386613!2d0.07097811577604497!3d51.5478449796418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a667ca0a7ec7%3A0x4c1df6f7141e124a!2sIlford+Lane+under+Fives!5e0!3m2!1sen!2sin!4v1495796516198" width="300" height="247" frameborder="0" style="border:0" allowfullscreen></iframe>
+			
+
+		</div>
+		</div>
+		</div>
+	</div>
+</div>
+<?php 
+require("footer.php");
+?>
